c6e58c7b603fb05493533542fc79158d329f21f2
Pavekatal <pavekatal@gmail.com>
2025-09-13 21:03:22 +0800

    замена роутера

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/Main";
 import SignInPage from "../pages/SignIn";
 import SignUpPage from "../pages/SignUp";
@@ -10,18 +10,20 @@ import PrivateRoute from "./private-route/PrivateRoute";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route element={<PrivateRoute />}>
-        <Route path="/" element={<MainPage />}>
-          <Route path="card/add" element={<NewCardPage />} />
-          <Route path="card/:id" element={<CardPage />} />
-          <Route path="exit" element={<ExitPage />} />
+    <Router>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<MainPage />}>
+            <Route path="card/add" element={<NewCardPage />} />
+            <Route path="card/:id" element={<CardPage />} />
+            <Route path="exit" element={<ExitPage />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="/login" element={<SignInPage />} />
-      <Route path="/register" element={<SignUpPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/register" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
